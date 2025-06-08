c3b8dc7ab4b9e7e952b15059d9166d09fd517e15
otobear <spacedogsergei@gmail.com>
2025-06-08 13:58:04 +0900

    change primevue theme

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from '@/app/App.vue'
 import router from '@/app/router'
@@ -10,6 +11,15 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+})
 
 app.mount('#app')
