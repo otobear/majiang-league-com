ee1550266b4958e569b0c001c8fc933060b14092
otobear <spacedogsergei@gmail.com>
2025-06-28 10:58:07 +0900

    use TheHeader in layout files

--- frontend/src/shared/ui/layouts/default-layout.vue
+++ frontend/src/shared/ui/layouts/default-layout.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="flex min-h-screen flex-col">
-    <header class="flex h-16 items-center bg-green-600 px-6 text-white shadow">
-      <img src="@/shared/assets/logo.png" alt="Logo" class="mr-4 h-9" />
-      <h1 class="flex-1 text-2xl font-bold">ちゅんま関東リーグ</h1>
-      <nav class="space-x-6">
-        <RouterLink
-          to="/"
-          class="border-b-2 border-transparent pb-1 transition-colors hover:border-yellow-200 hover:text-yellow-200"
-          >総合成績</RouterLink
-        >
-        <RouterLink
-          to="/players"
-          class="border-b-2 border-transparent pb-1 transition-colors hover:border-yellow-200 hover:text-yellow-200"
-          >選手一覧</RouterLink
-        >
-        <RouterLink
-          to="/tournaments"
-          class="border-b-2 border-transparent pb-1 transition-colors hover:border-yellow-200 hover:text-yellow-200"
-          >対局結果</RouterLink
-        >
-      </nav>
-    </header>
+    <TheHeader />
     <div class="flex flex-1 justify-center bg-gray-100 p-8">
       <main class="w-5xl xl:w-6xl">
         <slot />
@@ -28,3 +8,7 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { TheHeader } from '@/shared/ui/the-header'
+</script>
--- frontend/src/shared/ui/the-header/the-header.vue
+++ frontend/src/shared/ui/the-header/the-header.vue
@@ -1,10 +1,20 @@
 <template>
-  <header class="flex items-center justify-between px-4">
-    <RouterLink to="/" class="inline-flex h-16 items-center gap-4">
+  <header class="flex h-16 items-center bg-green-600 px-6 text-white shadow">
+    <RouterLink to="/" class="inline-flex h-16 flex-1 items-center gap-4">
       <img alt="Logo" src="@/shared/assets/logo.png" class="h-8 w-8" />
-      <div>
-        <p class="text-lg font-bold text-gray-800"></p>
-      </div>
+      <h1 class="text-2xl font-bold">ちゅんま関東リーグ</h1>
     </RouterLink>
+    <nav class="space-x-6">
+      <RouterLink
+        to="/players"
+        class="border-b-2 border-transparent pb-1 transition-colors hover:border-yellow-200 hover:text-yellow-200"
+        >総合成績</RouterLink
+      >
+      <RouterLink
+        to="/tournaments"
+        class="border-b-2 border-transparent pb-1 transition-colors hover:border-yellow-200 hover:text-yellow-200"
+        >対局結果</RouterLink
+      >
+    </nav>
   </header>
 </template>
